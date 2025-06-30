09684b47ab566a84752625b8d6009081b68bac4d
Sandro Reis <consultorsandro@hotmail.com>
2025-06-30 18:54:49 -0300

    Class 46

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+#![no_std] 
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::panic::PanicInfo; // class 46
+
+#[unsafe(no_mangle)]
+fn main() {
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { // never type ( ! ), because don't have control over the return
+    loop {}
+}
+/*
 #![no_std] // class 44
 fn main() {
 
     loop {}
 }
+*/
