a4c54ee096d4b980d1d1d2f5921d5b6fae257cf9
Sandro Reis <consultorsandro@hotmail.com>
2025-07-08 11:43:04 -0300

    Class 49 e 50

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 use core::panic::PanicInfo; // class 46
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // Not changing the name of the function Rust will generate
 fn main() {
 
     loop {}
@@ -14,6 +14,24 @@ fn main() {
 fn panic(_info: &PanicInfo) -> ! { // never type ( ! ), because don't have control over the return
     loop {}
 }
+/* // class 46
+#![no_std] 
+#![no_main]
+#![allow(clippy::empty_loop)]
+
+use core::panic::PanicInfo; // class 46
+
+#[unsafe(no_mangle)] // Not changing the name of the function Rust will generate
+fn main() {
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! { // never type ( ! ), because don't have control over the return
+    loop {}
+}
+*/
 /*
 #![no_std] // class 44
 fn main() {
---
+++ src/startup_stm32f303.rs
@@ -0,0 +1,14 @@
+
+// 1. Define the vector for the mcu
+
+
+// 2. Define the reset handler
+fn reset_handler() {
+
+    // 1. Copy the .data section from Flash to RAM
+    // 2. Zero out the .bss section in the RAM
+    // 3. Call main()
+
+}
+
+// 3. Define the execption handlers
